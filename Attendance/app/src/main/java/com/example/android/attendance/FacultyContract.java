2eb8aa3885b94d5a012ba90ae4b7820e37730026
Akshat Jain <jainakshat425@gmail.com>
2018-08-10 13:10:05 +0530

    FacultyContract Added

--- Attendance/app/src/main/java/com/example/android/attendance/FacultyContract.java
+++ Attendance/app/src/main/java/com/example/android/attendance/FacultyContract.java
@@ -1,4 +1,30 @@
 package com.example.android.attendance;
 
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 public class FacultyContract {
+    public static abstract class FacultyEntry implements BaseColumns {
+
+        public static final String CONTENT_AUTHORITY = "com.example.android.attendance";
+        public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+        public static final String PATH_FACULTY = "faculty";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI,PATH_FACULTY);
+
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_FACULTY;
+
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_FACULTY + "/#";
+
+        public static final String TABLE_NAME = "faculty";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String F_NAME_COL = "fac_name";
+        public static final String F_USERID_COL = "fac_user_id";
+        public static final String F_PASSWORD_COL = "fac_password";
+        public static final String F_DEPARTMENT_COL = "fac_department";
+
+    }
 }
